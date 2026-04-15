03e6aeb3e389b0ac5830fc157220811697fa6362
paolino <paolo.veronelli@gmail.com>
2026-04-15 17:58:01 +0100

    feat: add transaction construction via cardano-node-clients
    Add Cardano.Vouchers.Types (ToData instances matching Aiken encoding)
    and Cardano.Vouchers.Transaction (TxBuild program for voucher spends).
    
    Dependency changes:
    - Add cardano-node-clients, plutus-tx, cardano-ledger-api
    - Add CHaP repository and iohkNix overlays
    - Switch from GHC 9.10 to 9.8.4 (Cardano stack compatibility)

---
+++ offchain/src/Cardano/Vouchers/Types.hs
@@ -0,0 +1,65 @@
+-- | Voucher on-chain types with ToData instances matching the Aiken encoding.
+module Cardano.Vouchers.Types (
+    VoucherDatum (..)
+    , SpendRedeemer (..)
+    , Groth16Proof (..)
+) where
+
+import Data.ByteString (ByteString)
+import PlutusCore.Data qualified as PLC
+import PlutusTx.IsData.Class (ToData (..))
+import PlutusTx.Builtins.Internal (BuiltinData (..))
+
+-- | On-chain state per user.
+--
+-- Aiken: @Constr 0 [Int user_id, Int commit_spent]@
+data VoucherDatum = VoucherDatum
+    { vdUserId :: Integer
+    , vdCommitSpent :: Integer
+    }
+
+instance ToData VoucherDatum where
+    toBuiltinData (VoucherDatum uid cs) =
+        BuiltinData
+            $ PLC.Constr 0 [PLC.I uid, PLC.I cs]
+
+-- | Groth16 proof: three compressed BLS12-381 curve points.
+--
+-- Aiken: @Constr 0 [Bytes a, Bytes b, Bytes c]@
+data Groth16Proof = Groth16Proof
+    { gpA :: ByteString
+    -- ^ G1 element (48 bytes)
+    , gpB :: ByteString
+    -- ^ G2 element (96 bytes)
+    , gpC :: ByteString
+    -- ^ G1 element (48 bytes)
+    }
+
+instance ToData Groth16Proof where
+    toBuiltinData (Groth16Proof a b c) =
+        BuiltinData
+            $ PLC.Constr 0 [PLC.B a, PLC.B b, PLC.B c]
+
+-- | Redeemer for a spend transaction.
+--
+-- Aiken: @Constr 0 [Int d, Int commit_spent_new, Int issuer_ax, Int issuer_ay, Groth16Proof]@
+data SpendRedeemer = SpendRedeemer
+    { srD :: Integer
+    , srCommitSpentNew :: Integer
+    , srIssuerAx :: Integer
+    , srIssuerAy :: Integer
+    , srProof :: Groth16Proof
+    }
+
+instance ToData SpendRedeemer where
+    toBuiltinData (SpendRedeemer d csn iax iay proof) =
+        let BuiltinData proofData = toBuiltinData proof
+        in  BuiltinData
+                $ PLC.Constr
+                    0
+                    [ PLC.I d
+                    , PLC.I csn
+                    , PLC.I iax
+                    , PLC.I iay
+                    , proofData
+                    ]
